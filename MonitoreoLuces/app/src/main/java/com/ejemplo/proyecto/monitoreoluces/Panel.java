d92e131b6404fd806a2bf2c042aa8be70cbf2c53
drigox <rodrigosaez@MacBook-Pro-de-Rodrigo.local>
2015-02-04 17:31:38 -0300

    vista panel
    se crea la vista del panel y se realiza la transición entre inicio->
    panel

---
+++ MonitoreoLuces/app/src/main/java/com/ejemplo/proyecto/monitoreoluces/Panel.java
@@ -0,0 +1,18 @@
+package com.ejemplo.proyecto.monitoreoluces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Panel extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel);
+
+    }
+
+}
