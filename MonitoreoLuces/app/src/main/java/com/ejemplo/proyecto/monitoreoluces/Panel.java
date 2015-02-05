30c975a6d80671e52e5fd102d40c387ca0cea433
camilasalas1 <camilasalasortuzar@MacBook-Air-de-Camila.local>
2015-02-04 21:48:25 -0300

    Cambio a Toggle button

--- MonitoreoLuces/app/src/main/java/com/ejemplo/proyecto/monitoreoluces/Panel.java
+++ MonitoreoLuces/app/src/main/java/com/ejemplo/proyecto/monitoreoluces/Panel.java
@@ -5,13 +5,29 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 
 public class Panel extends ActionBarActivity {
+
+    private ToggleButton boton1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panel);
+
+
+        boton1=(ToggleButton) findViewById(R.id.boton1);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
 
     }
 
